497ec200d697ed793a3f85bd71199893fffebc69
SoSIE <sosie@sos-productions.com>
2020-10-30 15:03:13 +0100

    2.3.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,88 @@
-const path = require('path');
+/**
+ * Webpack configuration
+ *
+ * @author Codex Team
+ * @copyright Khaydarov Murod
+ */
+'use strict';
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader?fix=true']
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [
-                require('postcss-nested-ancestors'),
-                require('postcss-nested')
-              ]
+module.exports = (env, argv) => {
+  const path = require('path');
+  const pkg = require('./package.json');
+
+  /**
+   * Environment
+   *
+   * @type {any}
+   */
+  const NODE_ENV = argv.mode || 'development';
+  const VERSION = process.env.VERSION || pkg.version;
+
+  /**
+   * Plugins for bundle
+   *
+   * @type {webpack}
+   */
+  const webpack = require('webpack');
+
+
+    return  {
+    entry: [ './src/index.js' ],
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+            {
+                loader: 'babel-loader',
+                query: {
+                presets: [ '@babel/preset-env' ],
+                },
+            },
+	    'eslint-loader?fix=true'
+            ]
+        },
+        {
+            test: /\.css$/,
+            use: [
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                plugins: [
+                    require('postcss-nested-ancestors'),
+                    require('postcss-nested')
+                ]
+                }
             }
-          }
-        ]
-      },
-      {
-        test: /\.svg$/,
-        loader: 'svg-inline-loader?removeSVGTagAttrs=false'
-      }
-    ]
-  },
-  output: {
-    path: path.join(__dirname, '/dist'),
-    publicPath: '/',
-    filename: 'bundle.js',
-    library: 'LinkTool',
-    libraryExport: 'default',
-    libraryTarget: 'umd'
-  }
+            ]
+        },
+ 	{
+            test: /\.svg$/,
+            loader: 'svg-inline-loader?removeSVGTagAttrs=false'
+        }
+       ]
+    },
+     plugins: [
+      /** Pass variables into modules */
+      new webpack.DefinePlugin({
+        NODE_ENV: JSON.stringify(NODE_ENV),
+        VERSION: JSON.stringify(VERSION),
+      }),
+
+      new webpack.BannerPlugin({
+        banner: `Link tool\n\n@version ${VERSION}\n\n@package https://github.com/editor-js/link\n@licence MIT\n@author CodeX <https://codex.so>`,
+      }),
+     ],
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js',
+        library: 'LinkTool',
+        libraryTarget: 'umd',
+        libraryExport: 'default' // BEWARE this line was ommitted by me and create me no output,no error https://github.com/webpack/webpack/issues/3929
+    }
+  };
 };
