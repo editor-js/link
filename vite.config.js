fbce18873512caacbc8e2de96a69c3d3b671060a
robonetphy <1998patelumang@gmail.com>
2023-11-08 11:50:28 +0530

    name updated

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default {
     copyPublicDir: false,
     lib: {
       entry: path.resolve(__dirname, "src", "index.js"),
-      name: "Link",
+      name: "LinkTool",
       fileName: "link",
     },
   },
